2acc4bd8c74dde0e0a33e6cb9aa61432705089a1
spring-green <byazdani7@gmail.com>
2019-09-09 20:17:21 -0700

    Added  cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions create a special matrix and return its inverse
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a special matrix that would be inversable
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<- NULL
+  set<- function (y){
+    x<<- y
+    m<<- NULL
+  }
+  get<- function()x
+  setinverse<- function (inverse) m <<- inverse
+  getinverse<- function() m
+  list (
+    set= set, get=get,
+    getinverse=getinverse, setinverse=setinverse)
+  
+  }
 
-}
 
-
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the matrix created by makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<- x$getinverse()
+  if(!is.null(m)){
+    message("returning cached data")
+    
+    return(m)
+  }
+  mydata<- x$get()
+  m<- inverse(mydata)
+  x$getinverse(m)
+  m
 }
